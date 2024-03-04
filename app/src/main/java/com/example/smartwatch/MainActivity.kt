d18b252a403424734c8bf4f10c93a3b34b75cf2c
kristyson <131497756+kristyson@users.noreply.github.com>
2024-03-04 15:50:13 -0300

    Ajuste na formatação dos dados
    ao clicar no botão envia os dados para o firebase com a data do dispositivo como coleção, usa a hora como id do documento e uma mensagem genérica

--- app/src/main/java/com/example/smartwatch/MainActivity.kt
+++ app/src/main/java/com/example/smartwatch/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.smartwatch
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,23 +20,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inicialize o Firestore
+        // Initialize o Firestore
         db = FirebaseFirestore.getInstance()
 
-        // Adicione um novo documento com um ID gerado automaticamente.
+        val sendButton: Button = findViewById(R.id.btn_send)
+        sendButton.setOnClickListener {
+            sendDataToFirestore()
+        }
+    }
+
+    private fun sendDataToFirestore() {
+        // Obtenha a data e hora atual
+        val currentDateTime = Calendar.getInstance().time
+        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(currentDateTime)
+        val currentTime = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(currentDateTime)
+        val currentDocumentId = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(currentDateTime)
+
+        // Crie os dados a serem enviados sem os campos Date e Time
         val data = hashMapOf(
-            "name" to "John Doe",
-            "email" to "johndoe@example.com"
+            "CPF" to "080.199.323.12",
+            "Name" to "Samsung Smartwatch"
         )
 
-        db?.collection("users")
-            ?.add(data)
-            ?.addOnSuccessListener(OnSuccessListener<DocumentReference> { documentReference ->
+        // Adicione um novo documento com a hora atual como ID
+        db?.collection(currentDate)
+            ?.document(currentDocumentId)
+            ?.set(data)
+            ?.addOnSuccessListener {
+                // Se o envio for bem-sucedido, mostre uma mensagem de sucesso
+                showMessage("Mensagem enviada com sucesso!")
+            }
+            ?.addOnFailureListener { e ->
+                // Se ocorrer uma falha, mostre uma mensagem de erro
+                showMessage("Falha ao enviar mensagem: ${e.message}")
+            }
+    }
 
-                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.id)
-            })
-            ?.addOnFailureListener(OnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            })
+    private fun showMessage(message: String) {
+        // Mostra uma mensagem usando o Toast
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
